cd9e9e8294b4dc07e8caa66bbe33d6c450696687
Lukas Jaks <me@ljaks.com>
2025-12-01 10:46:31 +0100

    Add script to calculate total from input values
    This script reads from 'input.txt' and calculates a total based on the values prefixed with 'R' or 'L'. It adjusts the current value and counts how many times it reaches zero.

---
+++ d1.sh
@@ -0,0 +1,34 @@
+file="input.txt"
+
+cur=50
+
+total=0
+
+while read line; do
+    dir=${line:0:1}
+    amo=$(echo $line | cut -c 2-)
+
+    if [[ $dir = "R" ]]; then
+        cur=$(($cur + $amo))
+    else
+        cur=$(($cur - $amo))
+    fi
+
+    if [[ $cur -lt 0 ]]; then
+        cur=$(($cur * -1))
+        cur=$((100 - $cur))
+    fi
+
+    div=$(( $cur / 100 ))
+    if [[ $cur -ne 0 ]]; then
+        div=$(( $div * 100 ))
+        cur=$(($cur - div))
+    fi
+
+    if [[ $cur -eq 0 ]]; then
+        total=$(($total + 1))
+    fi
+done < $file
+
+echo $total
+echo "done"
